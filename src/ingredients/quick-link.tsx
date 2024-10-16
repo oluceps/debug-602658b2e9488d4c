9cd6f507a5eca7e7027d53d28e607378071ba358
oluceps <secinia@outlook.com>
2024-10-17 03:05:30 +0800

    -

--- src/ingredients/quick-link.tsx
+++ src/ingredients/quick-link.tsx
@@ -24,13 +24,7 @@ export const QuickLinks: ParentComponent<QuickLinksProps> = (props) => {
 	// 	setInnerW(0);
 	// };
 
-	// const innerWText = createMemo(() => `w-[${innerW()}%]`);
 
-	// onCleanup(() => clearTimeout(timeoutId));
-
-	// <a href={props.href} target="_blank">
-	// 	<span class="absolute -inset-px rounded-xl" />
-	// </a>
 	return (
 		<div/>
 	);

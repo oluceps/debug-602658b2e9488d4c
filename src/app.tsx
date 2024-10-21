b9bc03dff42e6df9bbf8f843c134750755fc550d
oluceps <secinia@outlook.com>
2024-10-21 14:13:43 +0800

    +

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,7 @@ import { ErrorBoundary, onMount, Suspense } from "solid-js";
 import { MDXProvider } from "solid-mdx";
 import Mdx from "./components/Mdx";
 import "./style.css";
-import { Layout } from "./components/Layout";
+// import { Layout } from "./components/Layout";
 import IErr from "./components/IErr";
 import NotFound from "./components/NotFound";
 import "virtual:uno.css";
@@ -27,11 +27,7 @@ export default function App() {
 							e.message == 404 ? <NotFound /> : <IErr>{e.message}</IErr>
 						}
 					>
-						<Layout>
-							<MDXProvider components={Mdx}>
-								<Suspense>{props.children}</Suspense>
-							</MDXProvider>
-						</Layout>
+						<div />
 					</ErrorBoundary>
 				</main>
 			)}

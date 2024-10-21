cebb9571ac26f8c66357088fd32c410a3772cae7
oluceps <secinia@outlook.com>
2024-10-21 14:30:20 +0800

    -

--- app.config.ts
+++ app.config.ts
@@ -1,28 +1,5 @@
 import { defineConfig } from "@solidjs/start/config";
 import UnoCSS from "unocss/vite";
-import remarkFrontmatter from "remark-frontmatter";
-import rehypeRaw from "rehype-raw";
-import { nodeTypes } from "@mdx-js/mdx";
-import remarkGfm from "remark-gfm";
-import rehypeShiki from '@shikijs/rehype'
-import remarkMath from 'remark-math'
-
-// @ts-expect-error missing types
-import rehypeTypst from '@myriaddreamin/rehype-typst'
-// @ts-expect-error missing types
-import remarkABCJS from 'remark-abcjs'
-
-import {
-	transformerNotationDiff,
-	transformerNotationHighlight,
-	transformerNotationFocus,
-	transformerNotationErrorLevel,
-	transformerNotationWordHighlight
-} from '@shikijs/transformers'
-
-// import rehypeExpressiveCode from "rehype-expressive-code";
-import rehypeSlug from "rehype-slug";
-import rehypeAutoLinkHeadings from "rehype-autolink-headings";
 
 
 // @ts-expect-error missing types
@@ -32,7 +9,6 @@ const { default: mdx } = pkg;
 
 
 export default defineConfig({
-	middleware: "src/middleware/index.ts",
 	extensions: ["mdx", "md", "tsx"],
 	vite: {
 		plugins: [
@@ -45,42 +21,6 @@ export default defineConfig({
 				jsx: true,
 				jsxImportSource: "solid-js",
 				providerImportSource: "solid-mdx",
-				rehypePlugins: [
-					[
-						rehypeRaw,
-						{
-							passThrough: nodeTypes,
-						},
-					],
-					[rehypeSlug],
-					[
-						rehypeAutoLinkHeadings,
-						{
-							behavior: "wrap",
-							properties: {
-								className: "heading",
-							},
-						},
-					],
-					rehypeTypst,
-					[rehypeShiki, {
-						inline: 'tailing-curly-colon',
-						theme: 'vitesse-light',
-						transformers: [
-							transformerNotationFocus(),
-							transformerNotationDiff(),
-							transformerNotationHighlight(),
-							transformerNotationErrorLevel(),
-							transformerNotationWordHighlight()
-						]
-					}],
-				],
-				remarkPlugins: [
-					remarkGfm,
-					remarkMath,
-					remarkABCJS,
-					remarkFrontmatter,
-				],
 			}),
 			{ enforce: "pre" },
 		],

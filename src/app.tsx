2d5246f5f72d7dc6604e3b3331a66a8187994b6b
oluceps <secinia@outlook.com>
2024-10-21 14:53:29 +0800

    -

--- src/app.tsx
+++ src/app.tsx
@@ -27,7 +27,7 @@ export default function App() {
 							</div>
 						}
 					>
-						<QuickLinks title="" icon={<div />} href="" />
+						<QuickLinks />
 					</ErrorBoundary>
 				</main>
 			)}

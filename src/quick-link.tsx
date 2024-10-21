ee617d66178a9cb11c3391dc5a457afd221b5aca
oluceps <secinia@outlook.com>
2024-10-21 14:53:09 +0800

    -

--- src/quick-link.tsx
+++ src/quick-link.tsx
@@ -1,22 +1,6 @@
 import { createMemo, createSignal, type JSXElement, onCleanup, type ParentComponent, Show } from "solid-js";
 
-import { Dynamic } from "solid-js/web";
-import { twMerge } from "tailwind-merge";
-
-export const isExternalURL = (url: string) =>
-	url.startsWith("https:") || url.startsWith("mailto:");
-
-export type QuickLinksProps = {
-	title: string;
-	href: string;
-	icon: JSXElement;
-	description?: string;
-	children?: JSXElement;
-	onlyIcon?: boolean;
-};
-
-
-export const QuickLinks: ParentComponent<QuickLinksProps> = (props) => {
+export const QuickLinks: ParentComponent<{}> = (props) => {
 	// const handleMouseLeave = () => {
 	// 	clearTimeout(timeoutId);
 	// 	setIsRelative(false);
@@ -32,6 +16,6 @@ export const QuickLinks: ParentComponent<QuickLinksProps> = (props) => {
 	// 	<span class="absolute -inset-px rounded-xl" />
 	// </a>
 	return (
-		<div/>
+		<div />
 	);
 };

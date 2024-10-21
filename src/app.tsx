a41356c55f690f8fbdb616cb6effac86a2d794b2
oluceps <secinia@outlook.com>
2024-10-21 14:16:53 +0800

    +

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,6 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
-import { ErrorBoundary, onMount, Suspense } from "solid-js";
-import { MDXProvider } from "solid-mdx";
-import Mdx from "./components/Mdx";
+import { ErrorBoundary } from "solid-js";
 import "./style.css";
 // import { Layout } from "./components/Layout";
 import IErr from "./components/IErr";
@@ -11,20 +9,14 @@ import "virtual:uno.css";
 import "@unocss/reset/tailwind-compat.css";
 
 export default function App() {
-	onMount(() => {
-		document.addEventListener("visibilitychange", () => {
-			if (document.visibilityState == "visible") {
-				navigator.wakeLock.request("screen");
-			}
-		});
-	});
 	return (
 		<Router
-			root={(props) => (
+			root={() => (
 				<main>
 					<ErrorBoundary
 						fallback={(e) =>
 							e.message == 404 ? <NotFound /> : <IErr>{e.message}</IErr>
+							// <div />
 						}
 					>
 						<div />

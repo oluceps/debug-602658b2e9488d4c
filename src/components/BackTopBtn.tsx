1a50aa9b071665e6320b3ce633e3d5b94478374b
oluceps <secinia@outlook.com>
2024-10-21 14:54:01 +0800

    -

--- src/components/BackTopBtn.tsx
+++ src/components/BackTopBtn.tsx
@@ -4,18 +4,7 @@ import { useLenis } from "lenis-solid";
 const ScrollTopBtn: Component = () => {
 	const scroll = useWindowScrollPosition();
 	return (
-		<>
-			<Show when={scroll.y > 350 && document.documentElement.clientWidth > 930}>
-				<button
-					onclick={() => {
-						useLenis().scrollTo(0)
-					}}
-					class="!fixed bottom-5 right-5 p-3 mb-3 bg-sprout-200/60 hover:bg-sprout-300/75 rounded-md h-9 w-9 grid items-center justify-center duration-200 h-9 w-9 flex"
-				>
-					<div class="i-ci-chevron-big-up" />
-				</button>
-			</Show>
-		</>
+		<div class="i-ci-chevron-big-up" />
 	);
 };
 

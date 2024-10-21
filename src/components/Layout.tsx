b1eda4a56afc9793a1fb580159216bcebb773c8b
oluceps <secinia@outlook.com>
2024-10-21 14:44:04 +0800

    -

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,21 +1,10 @@
 import { lazy, Suspense } from "solid-js";
-import {
-	Match,
-	type ParentProps,
-	Switch,
-	createEffect,
-	createSignal,
-} from "solid-js";
-import { useLocation } from "@solidjs/router";
-import { PageStateProvider, TaxoStateProvider } from "./PageState";
-import Me from "~/ingredients/me";
-import Footer from "./Footer";
-import { QuickLinks } from "~/ingredients/quick-link";
 
 const BackTopBtn = lazy(() => import("./BackTopBtn"));
-const Header = lazy(() => import("./Header"));
+import { QuickLinks } from "~/ingredients/quick-link";
 
-export function Layout(props: ParentProps) {
+
+export function Layout() {
 
 	return (
 
